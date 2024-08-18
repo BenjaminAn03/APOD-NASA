2968ef1aecb6869c3ac50743a4dca5cb3b338627
BenjaminAn03 <benjaminsitunes@gmail.com>
2024-08-18 01:17:50 -0500

    Handle picture or video

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,19 @@ export default function Main(props) {
 
     return (
         <div className="imgContainer">
-            <img src={data?.hdurl} alt={data?.title || "bg-image"} className="bgimage"/>
+            {data?.media_type === "image" ? (
+                <img src={data?.hdurl} alt={data?.title || "bg-image"} className="bgImage"/>
+            ) : data?.media_type === "video" ? (
+                <iframe
+                    src={data?.url}
+                    title={data?.title || "bg-video"}
+                    className="bgVideo"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
+            ) : (
+                <p>No media available</p>
+            )}
         </div>
-    )
+    );
 }
